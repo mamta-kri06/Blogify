93ef2ce4abf103a2a24a07136285ac673665ba1f
mamta <mamtakri230605@gmail.com>
2025-06-20 23:45:13 +0530

    second commit

--- beckend/index.js
+++ beckend/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.send("hello from server);
+  res.send("hello");
 });
 app.use("/auth", AuthRoutes);
 app.use("/blog", BlogRoutes);
