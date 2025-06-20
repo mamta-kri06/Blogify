5ab792820e0f57bec5c12b4e09d878077321b15c
mamta-kri06 <153258193+mamta-kri06@users.noreply.github.com>
2025-06-20 23:17:46 +0530

    Update index.js

--- beckend/index.js
+++ beckend/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.redirect(process.env.FRONTEND_URL);
+  res.send("hello from server);
 });
 app.use("/auth", AuthRoutes);
 app.use("/blog", BlogRoutes);
